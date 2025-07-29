0909bd126fd51bd9e168ca4789ebe7bd65e8816f
EmanSan9 <sanna950@gmail.com>
2025-07-29 15:34:53 +0200

    VFINAL

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,74 +1,62 @@
-import { Component } from "react";
 import SingleBook from "./SingleBook";
 import { Col, Form, Row } from "react-bootstrap";
 import CommentArea from "./CommentArea";
+import { useState } from "react";
 
-class BookList extends Component {
-  state = {
-    searchQuery: "",
-    selectedBookAsin: "",
-    // Elevo lo stato selected a BookList in origine era sul singolo libro, ogni volta che clicco su un libro si aggiorna questo stato
+const BookList = ({ books }) => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [selectedBookAsin, setSelectedBookAsin] = useState("");
+
+  const handleBookSelection = (asin) => {
+    setSelectedBookAsin(asin);
   };
 
-  handleBookSelection = (asin) => {
-    this.setState({ selectedBookAsin: asin });
-  };
-  // Metodo inserito che aggiorna selectedBookAsin nello stato
+  return (
+    <>
+      <Row className="justify-content-center mt-5">
+        <Col xs={12} md={4} className="text-center">
+          <Form.Group>
+            <Form.Control
+              type="search"
+              placeholder="Cerca un libro"
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+            />
+          </Form.Group>
+        </Col>
+      </Row>
 
-  render() {
-    return (
-      <>
-        <Row className="justify-content-center mt-5">
-          <Col xs={12} md={4} className="text-center">
-            <Form.Group>
-              <Form.Control
-                type="search"
-                placeholder="Cerca un libro"
-                value={this.state.searchQuery}
-                onChange={(e) => this.setState({ searchQuery: e.target.value })}
-              />
-            </Form.Group>
-          </Col>
-        </Row>
+      <Row className="mt-3">
+        <Col xs={8} md={8}>
+          <Row className="g-0 g-lg-3">
+            {books
+              .filter((b) =>
+                b.title.toLowerCase().includes(searchQuery.toLowerCase())
+              )
+              .map((b) => (
+                <Col xs={7} md={4} key={b.asin}>
+                  <SingleBook
+                    book={b}
+                    isSelected={selectedBookAsin === b.asin}
+                    onSelect={handleBookSelection}
+                  />
+                </Col>
+              ))}
+          </Row>
+        </Col>
 
-        <Row className="mt-3">
-          <Col xs={8} md={8}>
-            <Row className="g-0 g-lg-3">
-              {this.props.books
-                .filter((b) =>
-                  b.title
-                    .toLowerCase()
-                    .includes(this.state.searchQuery.toLowerCase())
-                )
-                .map((b) => (
-                  <Col xs={7} md={4} key={b.asin}>
-                    <SingleBook
-                      book={b}
-                      // Passo l'intero oggetto libro (con titolo, immagine, asin, ecc.
-                      isSelected={this.state.selectedBookAsin === b.asin}
-                      // Confronto l'asin del libro corrente (b.asin) con lo stato (selectedBookAsin) per capire se è selezionato.
-                      onSelect={this.handleBookSelection}
-                      // Passiamo la funzione handleBookSelection che si occupa di salvare l'asin selezionato nello state di BookList.
-                    />
-                  </Col>
-                ))}
-            </Row>
-          </Col>
-
-          <Col xs={4} md={4}>
-            {this.state.selectedBookAsin ? (
-              <CommentArea asin={this.state.selectedBookAsin} />
-            ) : (
-              // BookList passa selectedBookAsin come prop a CommentArea
-              <div className="text-center mt-5 text-muted">
-                <p>Seleziona un libro per visualizzare i commenti</p>
-              </div>
-            )}
-          </Col>
-        </Row>
-      </>
-    );
-  }
-}
+        <Col xs={4} md={4}>
+          {selectedBookAsin ? (
+            <CommentArea asin={selectedBookAsin} />
+          ) : (
+            <div className="text-center mt-5 text-muted">
+              <p>Seleziona un libro per visualizzare i commenti</p>
+            </div>
+          )}
+        </Col>
+      </Row>
+    </>
+  );
+};
 
 export default BookList;
--- src/components/CommentArea.jsx
+++ src/components/CommentArea.jsx
@@ -1,33 +1,23 @@
-import { Component } from "react";
 import CommentList from "./CommentList";
 import AddComment from "./AddComment";
 import Loading from "./Loading";
 import Error from "./Error";
+import { useState } from "react";
+import { useEffect } from "react";
 
-class CommentArea extends Component {
-  state = {
-    comments: [],
-    isLoading: false,
-    isError: false,
-  };
+const CommentArea = (props) => {
+  const [comments, setComments] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
 
-  componentDidMount() {
-    this.fetchComments();
-  }
+  const fetchComments = async () => {
+    if (!props.asin) return;
 
-  componentDidUpdate(prevProps) {
-    // CommentArea fa un fetch dei commenti ogni volta che riceve un asin nuovo
-    if (prevProps.asin !== this.props.asin) {
-      this.fetchComments();
-    }
-  }
-
-  fetchComments = async () => {
-    this.setState({ isLoading: true, isError: false });
+    setIsLoading(true);
+    setIsError(false);
     try {
-      let response = await fetch(
-        "https://striveschool-api.herokuapp.com/api/comments/" +
-          this.props.asin,
+      const response = await fetch(
+        "https://striveschool-api.herokuapp.com/api/comments/" + props.asin,
         {
           headers: {
             Authorization:
@@ -37,27 +27,31 @@ class CommentArea extends Component {
       );
 
       if (response.ok) {
-        let comments = await response.json();
-        this.setState({ comments, isLoading: false });
+        const comments = await response.json();
+        setComments(comments);
+        setIsLoading(false);
       } else {
         throw new Error("Errore nel fetch");
       }
     } catch (error) {
       console.error(error);
-      this.setState({ isLoading: false, isError: true });
+      setIsError(true);
+      setIsLoading(false);
     }
   };
+  useEffect(() => {
+    fetchComments();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [props.asin]);
 
-  render() {
-    return (
-      <div className="text-center">
-        {this.state.isLoading && <Loading />}
-        {this.state.isError && <Error />}
-        <AddComment asin={this.props.asin} />
-        <CommentList commentsToShow={this.state.comments} />
-      </div>
-    );
-  }
-}
+  return (
+    <div className="text-center">
+      {isLoading && <Loading />}
+      {isError && <Error />}
+      <AddComment asin={props.asin} />
+      <CommentList commentsToShow={comments} />
+    </div>
+  );
+};
 
 export default CommentArea;
